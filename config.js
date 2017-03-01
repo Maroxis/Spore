c59f79650c792a94f35d64511d3c9c81e2b841d0
Maroxis <wola1997@interia.pl>
2017-03-01 18:04:30 +0100

    clean

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-var cellSize = 20;
+var cellSize = 10;
+var mapSize = 400
+
 var inc = 0.006*cellSize;
 var det = 6;
+
 var maxFood = 15 // units
-var foodGrowRate = 10 // times/sec
-var mapSize = 600
+var foodGrowRate = 1 // times/sec
